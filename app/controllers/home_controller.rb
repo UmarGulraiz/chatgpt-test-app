3bd2f469c40beefd7cf35512e622759a662e8898
Umar Gulraiz <umar.gulraiz1@gmail.com>
2023-03-16 19:49:11 +0500

    minor improvements

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,12 +1,11 @@
 class HomeController < ApplicationController
+  before_action :configure_params
   def index
   end
 
    def get_suggestions
-    @year_level = params[:year_level]
-    @type_of_paragraph = params[:type_of_paragraph]
-    @essay_question = params[:essay_question]
-    @your_paragraph = params[:your_paragraph]
+    return unless execute_chatgpt_api?
+
     @suggestions = EssayCorrectionTool.new(
       @year_level,
       @type_of_paragraph,
@@ -14,5 +13,29 @@ class HomeController < ApplicationController
       @your_paragraph,
       "text-davinci-003",
     ).get_suggestions
+
+    reset_fields
   end
+
+  private
+
+  def configure_params
+    @year_level = params[:year_level]
+    @type_of_paragraph = params[:type_of_paragraph]
+    @essay_question = params[:essay_question]
+    @your_paragraph = params[:your_paragraph]
+    @suggestions = ""
+  end
+
+  def execute_chatgpt_api?
+    @year_level.present? && @type_of_paragraph.present? && @essay_question.present? && @your_paragraph.present?
+  end
+
+  def reset_fields
+    @year_level = "7"
+    @type_of_paragraph = "introduction"
+    @essay_question = ""
+    @your_paragraph = ""
+  end
+
 end
